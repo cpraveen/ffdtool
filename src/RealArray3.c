806b6c09121cff88c83232d0662159aea4934949
Praveen. C <cpraveen@gmail.com>
2010-10-15 07:51:26 +0000

    Fixed bug in for loop
    git-svn-id: https://subversion.assembla.com/svn/ffdtool@34 541b0c13-f016-46da-b177-33e518e44720

--- src/RealArray3.c
+++ src/RealArray3.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "type.h"
 
 REAL*** RealArray3(UINT nx, UINT ny, UINT nz){
    REAL ***x;
    UINT i, j;
 
+   assert(nx > 0 && ny > 0 && nz > 0);
+
    x = (REAL***)malloc(nx*sizeof(REAL**));
    if(x==NULL){
       printf("RealArray3: Could not allocate x\n");
+      printf("            size %d x %d x %d\n", nx, ny, nz);
       exit(0);
    }
    for(i=0; i<nx; i++){
       x[i] = (REAL**)malloc(ny*sizeof(REAL*));
       if(x[i]==NULL){
          printf("RealArray3: Could not allocate x[%d]\n",i);
+         printf("            size %d x %d x %d\n", nx, ny, nz);
          exit(0);
       }
-      for(j=0; j<ny; j++)
+      for(j=0; j<ny; j++){
          x[i][j] = (REAL*)malloc(nz*sizeof(REAL));
          if(x[i][j]==NULL){
             printf("RealArray3: Could not allocate x[%d][%d]\n",i,j);
+            printf("            size %d x %d x %d\n", nx, ny, nz);
             exit(0);
          }
+      }
    }
 
    return x;
